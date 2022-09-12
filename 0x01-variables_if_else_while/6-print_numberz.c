a832843cf9a8f58de5ea63e4671694cb943c74f5
mmuriuki311 <mmuriuki311@gmail.com>
2022-09-12 14:29:14 -0700

    Master

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 
 /**
- *   * main - Entry point
- *     *
- *       * Return: Always 0
- *         */
+ *   main - Entry point
+ *
+ *   Return: Always 0
+ */
+
 int main(void)
 {
 	int num;
@@ -13,4 +14,5 @@ int main(void)
 	putchar('\n');
 	return (0);
 }
+
 
