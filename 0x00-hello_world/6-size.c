eb51836ad4506d1b5003449a1e8414695918858d
mmuriuki311 <mmuriuki311@gmail.com>
2022-09-12 12:28:21 -0700

    Master

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
 /**
- *  * main - main block
- *   * Return: 0
- *    */
- 
+ * main - main block
+ * Return: 0
+ */
+
 int main(void)
 {
 		printf("Size of a char: %i byte(s)\n", sizeof(char));
