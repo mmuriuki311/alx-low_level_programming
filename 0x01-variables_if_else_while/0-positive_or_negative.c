6bbed30a635f467e3e934a074530527ecc71112d
mmuriuki311 <mmuriuki311@gmail.com>
2022-09-12 14:11:41 -0700

    Master

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+
+#include <time.h>
+
+/* Let's see if this works */
+
+
+/** 
+ * main - main block
+ * Return :0
+ */
+
+int main(void)
+
+{
+
+		int n;
+
+
+			srand(time(0));
+
+				n = rand() - RAND_MAX / 2;
+
+					/* your code goes there */
+
+					return (0);
+
+}
+
