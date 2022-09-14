6ede30fa8bdd06a9985d8184110871afe1eaa2dd
mmuriuki311 <mmuriuki311@gmail.com>
2022-09-14 15:06:59 -0700

    Master

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints alphabet in lowercase, followed by new line
+ * Return 0
+ */
+
+void print_alphabet(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter<='z'; letter++)
+	{
+		_putchar(letter);
+	}
+	_putchar('\n');
+}
