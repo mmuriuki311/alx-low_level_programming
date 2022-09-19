4c5e2a00a6acc27351692d0e96edd7f33897f381
mmuriuki311 <mmuriuki311@gmail.com>
2022-09-19 13:33:26 -0700

    Master

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -11,11 +11,8 @@ int main(void)
 
 {
 	int count;
-
 	unsigned long fib1 = 0, fib2 = 1, sum;
-
 	unsigned long fib1_half1, fib1_half2, fib2_half1, fib2_half2;
-
 	unsigned long half1, half2;
 
 	for (count = 0; count < 92; count++)
@@ -35,13 +32,11 @@ int main(void)
 	{
 		half1 = fib1_half1 + fib2_half1;
 		half2 = fib1_half2 + fib2_half2;
-
 		if (fib1_half2 + fib2_half2 > 9999999999)
 		{
 			half1 += 1;
 			half2 %= 10000000000;
 		}
-
 		printf("%lu%lu", half1, half2);
 
 		if (count != 98)
@@ -52,9 +47,7 @@ int main(void)
 		fib2_half1 = half1;
 		fib2_half2 = half2;
 	}
-
 	printf("\n");
-
 	return (0);
 
 }
